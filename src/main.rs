f9fc4ccebc612ddd31045efde25f31cfe3cc3fac
Richard Marbach <rmarbach@gmail.com>
2023-04-16 21:12:28 +0200

    Respond with pong

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
+
+struct RESPString {
+    value: String,
+}
+
+impl std::fmt::Display for RESPString {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "+{}\r\n", self.value)
+    }
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                stream.write(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
