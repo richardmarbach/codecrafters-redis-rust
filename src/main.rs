b5ebdc2dcc1d5919ddc79f6e0158297bc9cce3bb
Richard Marbach <rmarbach@gmail.com>
2023-04-22 23:41:17 +0200

    Parse resp

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,20 @@
-use std::future::Future;
+use redis_starter_rust::resp;
+use tokio::net::{TcpListener, TcpStream};
 
-use anyhow::{bail, Context};
-use tokio::{
-    io::{
-        AsyncBufRead, AsyncBufReadExt, AsyncRead, AsyncWrite, AsyncWriteExt, BufReader, BufWriter,
-        Lines,
-    },
-    net::{TcpListener, TcpStream},
-};
+async fn handle_connection(stream: TcpStream) -> anyhow::Result<()> {
+    let mut conn = resp::Connection::new(stream);
 
-enum RESP {
-    BulkString(BulkString),
-    SimpleString(String),
-    Array(Array),
-}
+    loop {
+        let Some(value) = conn.read_value().await? else { return Ok(()) };
+        println!("{:?}", value);
 
-struct Array {
-    data: Vec<RESP>,
-}
-
-impl Array {
-    async fn read<R: AsyncBufRead + Unpin>(r: &mut Lines<R>, len: &[u8]) -> anyhow::Result<Self> {
-        let len = std::str::from_utf8(len)?.parse::<usize>()?;
-        let mut data = Vec::with_capacity(len);
-        for _ in 0..len {
-            data.push(RESP::read(r).await?.context("incomplete array")?);
-        }
-        Ok(Array { data })
-    }
-}
-
-enum BulkString {
-    Data(String),
-    Empty,
-}
-
-impl std::fmt::Display for BulkString {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            BulkString::Data(text) => write!(f, "${}\r\n{}\r\n", text.len(), text),
-            BulkString::Empty => write!(f, "$-1\r\n\r\n"),
-        }
-    }
-}
-
-impl std::fmt::Display for RESP {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            RESP::BulkString(bulk_string) => write!(f, "{}", bulk_string),
-            RESP::SimpleString(string) => write!(f, "+{}\r\n", string),
-            RESP::Array(arr) => {
-                let len = arr.data.len();
-                write!(f, "*{}\r\n", len)?;
-                for item in arr.data.iter() {
-                    item.fmt(f)?;
-                }
-                write!(f, "\r\n")
+        match value.to_command()? {
+            resp::Command::Ping => {
+                let value = resp::Value::SimpleString("PONG".to_string());
+                conn.write_value(value).await?;
             }
         }
     }
-}
-
-impl RESP {
-    async fn read<R: AsyncBufRead + Unpin>(
-        r: &mut Lines<R>,
-    ) -> dyn Future<Output = anyhow::Result<Option<Self>>> {
-        let Some(line) = r.next_line().await? else {return Ok(None);};
-        let bytes = line.as_bytes();
-
-        match bytes[0] {
-            b'*' => Ok(Some(RESP::Array(Array::read(r, &bytes[1..]).await?))),
-            _ => bail!("Unknown type"),
-        }
-    }
-
-    async fn write<W: AsyncWrite + Unpin>(&self, w: &mut W) -> anyhow::Result<usize> {
-        w.write(format!("{}", &self).as_bytes())
-            .await
-            .map_err(Into::into)
-    }
-}
-
-async fn handle_connection(mut stream: TcpStream) -> anyhow::Result<()> {
-    let (reader, writer) = stream.split();
-    let reader = BufReader::new(reader);
-    let mut writer = BufWriter::new(writer);
-    let mut lines = reader.lines();
-
-    let command = RESP::read(&mut lines).await?;
-
-    // while let Some(line) = lines.next_line().await? {
-    //     println!("GOT: {}", line);
-    //     writer.write(b"+PONG\r\n").await?;
-    //     writer.flush().await?;
-    // }
-
-    Ok(())
 }
 
 #[tokio::main]
---
+++ src/resp.rs
@@ -0,0 +1,215 @@
+use anyhow::{anyhow, Result};
+use bytes::BytesMut;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
+
+pub struct Connection {
+    buffer: BytesMut,
+    inner: TcpStream,
+}
+
+impl Connection {
+    pub fn new(stream: TcpStream) -> Self {
+        Self {
+            buffer: BytesMut::with_capacity(4096),
+            inner: stream,
+        }
+    }
+
+    pub async fn read_value(&mut self) -> Result<Option<Value>> {
+        loop {
+            let bytes = self.inner.read_buf(&mut self.buffer).await?;
+
+            // Connection closed
+            if bytes == 0 {
+                return Ok(None);
+            }
+
+            if let Some((value, _)) = decode_message(&self.buffer.split())? {
+                return Ok(Some(value));
+            }
+        }
+    }
+
+    pub async fn write_value(&mut self, value: Value) -> Result<()> {
+        self.inner.write_all(value.encode().as_bytes()).await?;
+        Ok(())
+    }
+}
+
+fn decode_message(buffer: &[u8]) -> Result<Option<(Value, usize)>> {
+    match buffer[0] as char {
+        '+' => decode_simple_string(buffer),
+        '-' => decode_error(buffer),
+        ':' => decode_integer(buffer),
+        '$' => decode_bulk_string(buffer),
+        '*' => decode_array(buffer),
+        _ => Err(anyhow!("invalid RESP message")),
+    }
+}
+
+fn decode_array(buffer: &[u8]) -> Result<Option<(Value, usize)>> {
+    let Some((line, size)) = read_line(&buffer[1..]) else {
+        return Ok(None);
+    };
+
+    let Some(len) = read_number(&line) else {
+        return Err(anyhow!("invalid array"));
+    };
+
+    if len < 0 {
+        return Err(anyhow!("invalid array"));
+    }
+
+    let mut values = Vec::with_capacity(len as usize);
+
+    let mut offset = size + 1;
+    for _ in 0..len {
+        let Some((value, size)) = decode_message(&buffer[offset..])? else {
+            return Ok(None);
+        };
+        values.push(value);
+        offset += size;
+    }
+    Ok(Some((Value::Array(values), offset)))
+}
+
+fn decode_bulk_string(buffer: &[u8]) -> Result<Option<(Value, usize)>> {
+    let Some((line, size)) = read_line(&buffer[1..]) else {
+        return Ok(None);
+    };
+
+    let Some(len) = read_number(&line) else {
+        return Err(anyhow!("invalid bulk string"));
+    };
+
+    if len == -1 {
+        return Ok(Some((Value::Null, size)));
+    }
+
+    let start = size + 1;
+    if buffer.len() < start + (len as usize) + 2 {
+        return Err(anyhow!("bulk string too long"));
+    }
+
+    let string = String::from_utf8(buffer[start..start + len as usize].to_vec())?;
+    Ok(Some((Value::BulkString(string), size + (len as usize) + 2)))
+}
+
+fn decode_error(buffer: &[u8]) -> Result<Option<(Value, usize)>> {
+    let Some((line, size)) = read_line(&buffer[1..]) else {
+        return Ok(None);
+    };
+    let string = String::from_utf8(line.to_vec())?;
+    Ok(Some((Value::SimpleString(string), size)))
+}
+
+fn decode_simple_string(buffer: &[u8]) -> Result<Option<(Value, usize)>> {
+    let Some((line, size)) = read_line(&buffer[1..]) else {
+        return Ok(None);
+    };
+
+    let string = String::from_utf8(line.to_vec())?;
+    Ok(Some((Value::SimpleString(string), size)))
+}
+
+fn decode_integer(buffer: &[u8]) -> Result<Option<(Value, usize)>> {
+    let Some((line, size)) = read_line(&buffer[1..]) else {
+        return Ok(None);
+    };
+
+    let Some(num) = read_number(&line[1..]) else {
+        return Err(anyhow!("invalid integer"));
+    };
+    Ok(Some((Value::Integer(num), size)))
+}
+
+fn read_number(buffer: &[u8]) -> Option<i64> {
+    let mut size = 0;
+    let sign = if buffer[0] == b'-' { -1 } else { 1 };
+    let iter = if buffer[0] == b'-' {
+        buffer[1..].iter()
+    } else {
+        buffer.iter()
+    };
+
+    for byte in iter {
+        if byte < &b'0' || byte > &b'9' {
+            return None;
+        }
+
+        size = size * 10 + (byte - b'0') as usize;
+    }
+    return Some(sign * size as i64);
+}
+
+fn read_line(buffer: &[u8]) -> Option<(&[u8], usize)> {
+    for (i, &byte) in buffer.iter().enumerate() {
+        if byte == b'\r' && buffer.get(i + 1) == Some(&b'\n') {
+            return Some((&buffer[..i], i + 2));
+        }
+    }
+    return None;
+}
+
+#[derive(Debug)]
+pub enum Value {
+    SimpleString(String),
+    Error(String),
+    Integer(i64),
+    BulkString(String),
+    Null,
+    Array(Vec<Value>),
+}
+
+impl Value {
+    pub fn to_command(&self) -> Result<Command> {
+        match self {
+            Value::Array(values) => {
+                let mut iter = values.iter();
+                let name = match iter.next() {
+                    Some(Value::BulkString(s)) => s,
+                    _ => return Err(anyhow!("invalid command")),
+                };
+
+                let mut args: Vec<&str> = Vec::new();
+                for value in iter {
+                    match value {
+                        Value::BulkString(ref s) => args.push(s),
+                        _ => return Err(anyhow!("invalid command")),
+                    }
+                }
+
+                match name.to_ascii_lowercase().as_str() {
+                    "ping" => Ok(Command::Ping),
+                    _ => return Err(anyhow!("invalid command")),
+                }
+            }
+            _ => Err(anyhow!("invalid command")),
+        }
+    }
+
+    fn encode(&self) -> String {
+        match self {
+            Value::SimpleString(s) => format!("+{}\r\n", s),
+            Value::Null => "$-1\r\n".to_string(),
+            Value::Integer(i) => format!(":{}\r\n", i),
+            Value::BulkString(s) => format!("${}\r\n{}\r\n", s.len(), s),
+            Value::Error(s) => format!("-{}\r\n", s),
+            Value::Array(values) => {
+                let mut string = format!("*{}\r\n", values.len());
+                for value in values {
+                    string.push_str(&value.encode());
+                }
+                string
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum Command {
+    Ping,
+}
